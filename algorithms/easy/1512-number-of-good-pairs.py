2c8464c42348b4ab33e27d593fbc48a38411bb40
atbee <atb@odds.team>
2021-05-18 22:58:49 +0700

    Submit 1512 problem :bee:

---
+++ algorithms/easy/1512-number-of-good-pairs.py
@@ -0,0 +1,43 @@
+"""
+https://leetcode.com/problems/number-of-good-pairs/
+
+Given an array of integers nums.
+
+A pair (i,j) is called good if nums[i] == nums[j] and i < j.
+
+Return the number of good pairs.
+
+
+Example 1:
+Input: nums = [1,2,3,1,1,3]
+Output: 4
+Explanation: There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-indexed.
+
+Example 2:
+Input: nums = [1,1,1,1]
+Output: 6
+Explanation: Each pair in the array are good.
+
+Example 3:
+Input: nums = [1,2,3]
+Output: 0
+ 
+
+Constraints:
+
+1 <= nums.length <= 100
+1 <= nums[i] <= 100
+"""
+
+class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        numPairs = {}
+        result = 0
+        for num in nums:
+            if num in numPairs:
+                result += numPairs[num]
+                numPairs[num] += 1
+            else:
+                numPairs[num] = 1
+
+        return result
